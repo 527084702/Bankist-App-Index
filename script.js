a0bebe52517995c33eb67d467135cc6e239330f3
RexHuang <a527084702@gmail.com>
2022-12-24 11:52:06 +1100

    Small update
    Update html, js and css for link between main index and login page.

--- script.js
+++ script.js
@@ -77,8 +77,14 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
   // CHeck/Matching
   if (e.target.classList.contains('nav__link')){
     const id = e.target.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth'});
+    if (id !== '#'){
+      console.log(id);
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth'});
+    }
+  }
+  if (e.target.classList.contains('nav__link_login')){
+    //const id = e.target.getAttribute('href');
+    window.location = ('https://527084702.github.io/Bankist-App/');
   }
 })
 
